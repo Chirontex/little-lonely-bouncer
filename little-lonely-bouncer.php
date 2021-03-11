04d14e39f5e34db7ab556ca2741afcbf22796591
Dmitry Shumilin <infernozzz@yandex.ru>
2021-03-11 13:18:16 +0300

    0.0.1: initial

---
+++ little-lonely-bouncer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Little Lonely Bouncer
+ * Plugin URI: https://github.com/chirontex/little-lonely-bouncer
+ * Description: Этот плагин позволяет реализовать простейшую проверку для доступа к странице.
+ * Version: 0.0.1
+ * Author: Дмитрий Шумилин
+ * Author URI: mailto://chirontex@yandex.com
+ */
+
